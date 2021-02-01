1230bba39a2ce2877c12bff8e9d08893925faa9a
Jasper van Rijbroek <jasper@jvar.nl>
2021-02-01 16:21:13 +0100

    Cleaned up index.ts file

--- index.ts
+++ index.ts
@@ -1,69 +1,7 @@
 import 'reflect-metadata';
-// import AnnouncementsController from './app/controllers/announcements';
-// import CarsController from './app/controllers/cars';
-// import CatalogController from './app/controllers/catalog';
-// import ChatController from './app/controllers/chat';
-// import EventsController from './app/controllers/events';
-// import GiftsController from './app/controllers/gifts';
-// import LoggingController from './app/controllers/logging';
-// import NewsController from './app/controllers/news';
-// import PersonaController from './app/controllers/persona';
-// import ReportingController from './app/controllers/reporting';
-// import SecurityController from './app/controllers/security';
-// import SocialController from './app/controllers/social';
-// import SystemController from './app/controllers/system';
-// import UserController from './app/controllers/user';
-// import {IRouteDefinition} from './app/decorators/routing';
-// import {parseXMLBody} from './app/middleware/parseXML';
-// import { User } from './app/models/user';
-
-import './lib/game';
 import Game from './lib/game';
 
 global.debug = require('debug')('nfsw:bootstrap');
 
 Game.getInstance()
     .start()
-
-// createConnection().then(() => {
-//     global.debug('Database connection created!');
-// });
-
-// app
-//     .use(parseXMLBody)
-//     .use(staticPath(`${__dirname}/static`))
-//     .use((req: Request, res: Response, next: Function) => {
-//         global.debug(`${req.method} to ${req.url}`);
-
-//         return next();
-//     });
-
-// // Collect all the routes from the controllers
-// [
-//     AnnouncementsController,
-//     CarsController,
-//     CatalogController,
-//     ChatController,
-//     EventsController,
-//     GiftsController,
-//     LoggingController,
-//     NewsController,
-//     PersonaController,
-//     ReportingController,
-//     SecurityController,
-//     SocialController,
-//     SystemController,
-//     UserController
-// ].forEach((Controller: any) => {
-//     
-// });
-
-// app.use((req: Request, res: Response, next: Function) => {
-//     global.debug(`Missing handler for ${req.method} to ${req.url}`);
-
-//     return res.json('');
-// });
-
-
-// app.set('etag', false);
-// app.disable('x-powered-by');
