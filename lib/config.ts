f027a20bf429157d031bf52cc0d6fc3e5d7b4cc7
Jasper van Rijbroek <jasper@jvar.nl>
2021-02-16 07:28:39 +0100

    Updated config detection.

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,13 @@
+import { existsSync, writeFileSync } from "fs";
+
 export class Config {
-    static config: any = null;
+    static config: any = {};
+    static filePath = `${process.cwd()}/config.json`;
 
     static getConfig(): any {
-        if(!this.config) {
+        if(!Object.keys(this.config).length) {
             try {
-                this.config = require(`${process.cwd()}/config.json`);
+                this.config = require(this.filePath);
             } catch(err) { /* NOOP */ }
         }
 
@@ -19,5 +22,32 @@ export class Config {
 
             return carry[key];
         }, this.getConfig());
+    }
+
+    static set(key: string, value: any): Config {
+        let config = this.getConfig(),
+            parts = key.split('.'),
+            name = parts.pop() as string,
+            base = parts.reduce((carry: any, key: string) => {
+                if(!carry[key]) {
+                    carry[key] = {};
+                }
+
+                return carry[key];
+            }, config);
+
+        base[name] = value;
+
+        return this;
+    }
+
+    static save(): Config {
+        writeFileSync(this.filePath, JSON.stringify(this.config));
+
+        return this;
+    }
+
+    static check(): boolean {
+        return existsSync(this.filePath);
     }
 }
