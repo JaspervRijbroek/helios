f849f53ccae1c9b5212f942fc5a98e0eac9f4148
Jasper van Rijbroek <jasper@jvar.nl>
2021-02-16 07:51:23 +0100

    Updated CLI with a user choice.

---
+++ bin/cli-features.ts
@@ -0,0 +1,8 @@
+export default class CliSetup {
+    message: string = 'Manage server features';
+    priority: number = 20;
+
+    async execute(): Promise<void> {
+        return;
+    }
+}
--- bin/cli.ts
+++ bin/cli.ts
@@ -1,6 +1,7 @@
 import { blue, green, yellow } from "chalk";
 import { existsSync, fstat } from "fs";
 import { sync } from "glob";
+import { prompt } from "inquirer";
 import { parse } from "path";
 
 console.log(yellow('Welcome to') + green('  _ _               ___   __   _____ '));
@@ -12,23 +13,55 @@ console.log();
 
 (async () => {
     let configPath = `${process.cwd()}/config.json`,
-        commands = sync(`${__dirname}/cli-*.ts`)
-            .reduce((carry: any, pathName: string) => {
-                let parts = parse(pathName),
-                    fileName = parts.name.replace('cli-', ''),
-                    Module = require(pathName).default;
+        items = sync(`${__dirname}/cli-*.ts`).map((pathName: string) => {
+            let parts = parse(pathName),
+                fileName = parts.name.replace('cli-', ''),
+                Module = require(pathName).default,
+                module = new Module();
 
-                carry[fileName] = new Module();
+            return {
+                name: module.message,
+                value: fileName,
+                module
+            }
+        }),
+        commands = items
+            .reduce((carry: any, item: any) => {
+                carry[item.value] = item.module;
 
                 return carry;
             }, {});
+
+    items.sort((a: any, b: any) => {
+        if(a.module.priority == b.module.priority) {
+            return 0;
+        }
+
+        return a.module.priority < b.module.priority ? -1 : 1;
+    });
+
+    items.push({
+        name: 'Abort',
+        value: '',
+        module: false
+    });
 
     if (!existsSync(configPath)) {
         // Calling setup.
         console.log(blue('Config file is missing, running first-time-setup!'));
         await commands['setup'].execute();
     } else {
+        let answers = await prompt([{
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: items
+        }]);
 
+        if(!answers.action) {
+            console.log(green('Aborting!'));
+            return;
+        }
     }
 
     console.log(green('All Done!'));
