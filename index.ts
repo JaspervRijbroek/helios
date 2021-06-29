7f68cfd4a9f5cb919caf5df0dadf6eec213368a8
Jasper van Rijbroek <jasper@jvar.nl>
2021-06-29 09:35:19 +0200

    Added ascii art

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
 import Game from "./src/game";
+import {yellow, green, red, bold} from 'chalk';
+import debug from "debug";
+
+console.log(yellow('Welcome to') + green('  _ _'))
+console.log(green('  /\\  /\\___| (_) ___  ___ '));
+console.log(green(' / /_/ / _ \\ | |/ _ \\/ __|'));
+console.log(green('/ __  /  __/ | | (_) \\__ \\'));
+console.log(green('\\/ /_/ \\___|_|_|\\___/|___/'));
+console.log((debug.enabled('nfsw:game') ? red(bold('Debug mode!')) : '           ') + yellow('        A NFS: World Server'));
+console.log();
 
 new Game()
     .loadConfig()
